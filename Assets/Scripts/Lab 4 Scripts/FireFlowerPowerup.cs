cfa5abcf8180210c40a103d7b3d6380084d9ef4a
ioitami <ioitami@protonmail.com>
2023-10-09 16:00:36 +0800

    fireflower script added

--- Assets/Scripts/Lab 4 Scripts/FireFlowerPowerup.cs
+++ Assets/Scripts/Lab 4 Scripts/FireFlowerPowerup.cs
@@ -1,18 +1,84 @@
+
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class FireFlowerPowerup : MonoBehaviour
+public class FireflowerPowerup : BasePowerup
 {
-    // Start is called before the first frame update
-    void Start()
+    // setup this object's type
+    // instantiate variables
+    public GameConstants SMB;
+
+    protected override void Start()
     {
-        
+        base.Start(); // call base class Start()
+        this.type = PowerupType.FireFlower;
+        this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        this.GetComponent<Rigidbody2D>().simulated = true;
+        //StartCoroutine(ActiveObject());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ActiveObject()
     {
-        
+        yield return new WaitWhile(() => this.GetComponent<Rigidbody2D>().bodyType == RigidbodyType2D.Static);
+        this.GetComponent<Rigidbody2D>().simulated = true;
+        rigidBody.AddForce(Vector2.right * 3, ForceMode2D.Impulse); // move to the right
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player") && spawned)
+        {
+            // TODO: do something when colliding with Player
+
+            // then destroy powerup (optional)
+            gameObject.SetActive(false);
+
+        }
+        else if (col.gameObject.layer == 10) // else if hitting Pipe, flip travel direction
+        {
+            if (spawned)
+            {
+                goRight = !goRight;
+                rigidBody.AddForce(Vector2.right * 3 * (goRight ? 1 : -1), ForceMode2D.Impulse);
+
+            }
+        }
+    }
+
+    // interface implementation
+    public override void SpawnPowerup()
+    {
+        GameObject.Find("Mushroom").GetComponent<Animator>().SetBool("isSpawned", true);
+        this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        spawned = true;
+    }
+
+    public void GameRestart()
+    {
+        Debug.Log("Game reset FLOWER");
+        gameObject.SetActive(true);
+        GameObject.Find("FIreflower").GetComponent<Animator>().SetBool("isSpawned", false);
+        this.transform.localPosition = new Vector3(0f, 0f, 0f);
+        this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        this.GetComponent<Rigidbody2D>().simulated = false;
+        spawned = false;
+
+        //StartCoroutine(ActiveObject());
+        //GameObject.Find("Fireflower").GetComponent<Animator>().SetBool("isSpawned", false);
+    }
+
+    // interface implementation
+    public override void ApplyPowerup(MonoBehaviour i)
+    {
+        // TODO: do something with the object
+        //base.ApplyPowerup(i);
+        // try
+        MarioStateController mario;
+        bool result = i.TryGetComponent<MarioStateController>(out mario);
+        if (result)
+        {
+            mario.SetPowerup(this.powerupType);
+        }
     }
 }
